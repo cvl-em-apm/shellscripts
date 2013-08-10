0549ccb58366721fdac749f03daf8ac84d28ff34
Sean Lin <seanl@intersect.org.au>
2013-08-10 23:08:59 +1000

    Change functions to cvl_emap_functions

--- test/MRF-launcher.sh
+++ test/MRF-launcher.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-. /usr/local/shellscripts/0.1.2/bin/functions
+
+# cvl_emap shell functions
+. /usr/local/share/cvl_emap_functions
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 log_start
 script -q -c "$DIR/MRF 2>'$stderr_file_name'" --timing="$timing_file_name" "$stdio_file_name"
